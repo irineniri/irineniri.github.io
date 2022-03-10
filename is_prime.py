b22afc0c5bf9a0c645097defe46f542924c6a89c
irineniri <99204036+irineniri@users.noreply.github.com>
2022-03-10 16:14:02 +0300

    Create is_prime.py

---
+++ is_prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+
+    if n > 1:
+
+        for i in range(2, int(n/2)+1):
+    
+            if (n % i) == 0:
+                return False
+                break
+            else:
+                return True
+  
+    else:
+         return False
